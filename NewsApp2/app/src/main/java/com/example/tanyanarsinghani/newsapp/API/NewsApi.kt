b8d2b966b7d2eee6feddf81e440297262205cce0
Tanya Narsinghani <tanyanarsinghani01@gmail.com>
2019-03-07 14:29:50 +0530

    Change1

--- NewsApp2/app/src/main/java/com/example/tanyanarsinghani/newsapp/API/NewsApi.kt
+++ NewsApp2/app/src/main/java/com/example/tanyanarsinghani/newsapp/API/NewsApi.kt
@@ -17,14 +17,4 @@ interface NewsApi {
    fun getAllHighlights():retrofit2.Call<NewsArticles>
     @GET("top-headlines")
     fun getSportsNews(@Query("country") country:String, @Query("category") category:String,@Query("apikey") apikey:String):retrofit2.Call<NewsArticles>
-    @GET("top-headlines?country=in&category=entertainment&apiKey=2c76206f105a485ba35cb0af49de7805")
-    fun getEntertainmentNews():retrofit2.Call<NewsArticles>
-    @GET("top-headlines?country=in&category=business&apiKey=2c76206f105a485ba35cb0af49de7805")
-    fun getBussinessNews():retrofit2.Call<NewsArticles>
-    @GET("top-headlines?country=in&category=health&apiKey=2c76206f105a485ba35cb0af49de7805")
-    fun getHealthNews():retrofit2.Call<NewsArticles>
-    @GET("top-headlines?country=in&category=science&apiKey=2c76206f105a485ba35cb0af49de7805")
-    fun getScienceNews():retrofit2.Call<NewsArticles>
-    @GET("top-headlines?country=in&category=technology&apiKey=2c76206f105a485ba35cb0af49de7805")
-    fun getTechnologyNews():retrofit2.Call<NewsArticles>
 }
